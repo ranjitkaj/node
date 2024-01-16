fefe35e3405341f5eedc3a5e3121a5fa04bc4477
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-01-16 11:45:50 +0530

    Update app.js

--- new/app.js
+++ new/app.js
@@ -1,19 +1,22 @@
 a = require("node:fs")
 
-console.log('ushboo')
-console.log('This  Patna')
+// console.log('ushboo')
+// console.log('This  Patna')
 
- b = a.readFileSync("./d.txt",'utf-8')
- console.log(b)
-console.log('Rkhushboo')
-console.log('This is NSTI Patna')
+//  b = a.readFileSync("./d.txt",'utf-8')
+//  console.log(b)
+// console.log('Rkhushboo')
+// console.log('This is NSTI Patna')
 
-a.readFile("./c.txt", 'utf-8', (error, data) =>{
+// a.readFile("./c.txt", 'utf-8', (error, data) =>{
 
-    if(error){console.log(error)}
-    else{console.log(data)}
+//     if(error){console.log(error)}
+//     else{console.log(data)}
 
-})
+// })
 
-console.log('mukesh')
-console.log('Patna')
+// console.log('mukesh')
+// console.log('Patna')
+
+a.writeFileSync('./e.txt', 'This is Patna.')
+
