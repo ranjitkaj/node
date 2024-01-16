9beb95acbc651c8e85fab81519ee327788a03bc9
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-01-16 16:04:50 +0530

    Update index.js

--- new/index.js
+++ new/index.js
@@ -1,15 +1,24 @@
 http = require('node:http')
 fs = require('node:fs')
+// Server = http.createServer((req, res)=>{
+//     data = {name:'Ranjit', location:'Patna', age:23}
+//     res.writeHead(200, {'Content-type':'application/json'})
+//     res.end(JSON.stringify(data))
+//     html = fs.readFileSync('./index.html','utf8')
+
+//     res.writeHead(200, {'Content-type':'text/html'})
+//     res.end(req.url)
+
+// })
+
 Server = http.createServer((req, res)=>{
-    // data = {name:'Ranjit', location:'Patna', age:23}
-    // res.writeHead(200, {'Content-type':'application/json'})
-    // res.end(JSON.stringify(data))
-
-
-    html = fs.readFileSync('./index.html','utf8')
-
-    res.writeHead(200, {'Content-type':'text/html'})
-    res.end(html)
+    index = fs.readFileSync('./index.html','utf8')
+    contact = fs.readFileSync('./contact.html','utf8')
+    education = fs.readFileSync('./EDUCATION.html','utf8')
+    if(req.url==='/'){res.end(index)}
+    if(req.url==='/contact'){res.end(contact)}
+    if(req.url==='/education'){res.end(education)}
+    
 
 })
 
