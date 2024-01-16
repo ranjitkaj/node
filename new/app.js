381df5524648de4e75b7aa061731c5dc48f01cc4
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-01-16 11:35:33 +0530

    Update app.js

--- new/app.js
+++ new/app.js
@@ -1,3 +1,12 @@
 a = require("node:fs")
-b = a.readFileSync("./c.txt",'utf-8')
-console.log(b)
+// b = a.readFileSync("./c.txt",'utf-8')
+// console.log(b)
+
+a.readFile("./c.txt", 'utf-8', (error, data) =>{
+
+    if(error){console.log(error)}
+    else{console.log(data)}
+
+})
+
+    
