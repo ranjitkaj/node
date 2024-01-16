e8a6c61177859659bc89ce525c70b5db74873036
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-01-16 15:20:46 +0530

    Update index.js

--- new/index.js
+++ new/index.js
@@ -1,7 +1,15 @@
 http = require('node:http')
+fs = require('node:fs')
 Server = http.createServer((req, res)=>{
-    res.writeHead(200, {'Content-type':'text/plain'})
-    res.end('<h1>This is Body.</h1>')
+    // data = {name:'Ranjit', location:'Patna', age:23}
+    // res.writeHead(200, {'Content-type':'application/json'})
+    // res.end(JSON.stringify(data))
+
+
+    html = fs.readFileSync('./index.html','utf8')
+
+    res.writeHead(200, {'Content-type':'text/html'})
+    res.end(html)
 
 })
 
