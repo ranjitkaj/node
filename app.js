53af09e7bb6c036fee1ea1612bc8aa45657463a1
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-01-18 13:48:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
-a = require("./module.js")
+// a = require("./module.js")
 
-console.log("hi....i I am Ranjit from NSTI Patna")
+// console.log("hi....i I am Ranjit from NSTI Patna")
 
-require('./module')
+// require('./module')
+
+
+fs = require("node:fs")
+
+// fs.writeFile('./f.txt', 'I am Ranjit from NSTI Allahabad.', {flag: "a"}, (error) =>{
+//     if(error){
+//         console.log(error)
+//     }
+//     else{
+//         console.log('Successfully Created.')
+//     }
+// })
+
+fs.writeFileSync('./rait.txt', 'hello Mukesh')
 
