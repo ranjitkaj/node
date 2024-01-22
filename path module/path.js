aa8fef52e396d6832271f63ec7e520c074d8300a
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-01-22 16:15:49 +0530

    Update path.js

--- path module/path.js
+++ path module/path.js
@@ -1,29 +1,35 @@
 a = require('node:path')
 
-console.log(__filename)
 
-console.log(__dirname)
-
-console.log(a.basename(__filename))
-
-console.log(a.basename(__dirname))
-
-console.log(a.extname(__filename))
+console.log(a.isAbsolute(__filename))
 
 
-console.log(a.parse(__filename))
 
 
-console.log(a.parse(__dirname))
+// console.log(__filename)
+
+// console.log(__dirname)
+
+// console.log(a.basename(__filename))
+
+// console.log(a.basename(__dirname))
+
+// console.log(a.extname(__filename))
 
 
-b = 25
+// console.log(a.parse(__filename))
 
-const count = b;
-console.log('count: %d', count);
 
-console.log('count:', count);
+// console.log(a.parse(__dirname))
 
-console.log('count: %d', count);
 
-console.log('count:', count);
+// b = 25
+
+// const count = b;
+// console.log('count: %d', count);
+
+// console.log('count:', count);
+
+// console.log('count: %d', count);
+
+// console.log('count:', count);
