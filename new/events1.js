0870fc5789149f4f3878197424a06ba4cde43670
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-01-23 15:27:59 +0530

    Update events1.js

--- new/events1.js
+++ new/events1.js
@@ -10,4 +10,8 @@ emitter.on('order-cake', (type, flavour, size)=> {
 
 class cakeShop{
     constucter(){this.ordernumber = 0 }
+
+    getordernumber(){console.log('We are working on ',  this.ordernumber++)}
+
+
 }
