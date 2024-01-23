4bed1c3d92f2e39c10dd85bdf3ae0a7a8a88fc68
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-01-23 15:19:30 +0530

    Update events1.js

--- new/events1.js
+++ new/events1.js
@@ -6,3 +6,8 @@ emitter = new eventsemitter()
 emitter.on('order-cake', (type, flavour, size)=> {
     console.log('order recived!! baking ' )
 })
+
+
+class cakeShop{
+    constucter(){this.ordernumber = 0 }
+}
