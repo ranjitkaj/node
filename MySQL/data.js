ba3b37a6466ac3c629dce09d3396365315e9e989
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-01-25 16:40:26 +0530

    Update data.js

--- MySQL/data.js
+++ MySQL/data.js
@@ -2,11 +2,16 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : ''
+  password : '',
+  database : 'studentdb'
 });
 
 connection.connect((err)=>{
     if(err) throw err;
     else (console.log('successfull'))
-    a = "insert into patna(name, mail, age) values("
+    a = "insert into student_data values(1, 'Ranjit', 22, 'ranjitkajraitha')"
+    connection.query(a, (err, res)=>{
+      if (err) throw err
+      console.log('data inserted successfully!!!')
+    })
 })
