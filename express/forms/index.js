de85f74794c5cafd93b711f05669c6e742f22847
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-01-25 14:33:48 +0530

    d

--- express/forms/index.js
+++ express/forms/index.js
@@ -4,11 +4,11 @@ fs = require('fs');
 bodyParser = require('body-parser');
 app = express();
 
-app = express();
+
 port = 3000;
 
-bodyParser = require('bodyParser');
-app.use(bodyParser.urlencoded({ extended: true}))
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, ()=>{
     console.log(`server running on port ${port}`)
@@ -18,7 +18,7 @@ app.listen(port, ()=>{
 app.get('/', (req,res)=>{res.sendFile(path.join(__dirname, 'index.html'))})
 
 app.post('/submit', (req,res)=>{
-    details = res.body.fname + " " + req.body.location;
+    details = req.body.fname + " " + req.body.location;
     fs.writeFile('details.txt', details, (err)=>{
         if(err) {
             console.log(err)
@@ -27,5 +27,5 @@ app.post('/submit', (req,res)=>{
             console.log('file created')
         }
     })
-    res.send('Thanqu', req.body.fname)
+    res.send('Thanqu')
 })
