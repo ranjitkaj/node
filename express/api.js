a83b625e07987bfe7e7a7dc181295eac36238941
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-01-25 10:35:47 +0530

    Update api.js

--- express/api.js
+++ express/api.js
@@ -2,3 +2,15 @@ express = require('express');
 
 app = express();
 port = 3000;
+
+app.listen(port, ()=>{
+    console.log(`server running on port `)
+
+})
+app.get('/', (req,res)=>{res.send('<html lang="en"><body><h1>This is nsti.</h1></body></html>')})
+
+app.post('/submit-data', (req,res)=>{res.send('<html lang="en"><body><h1>I am Ranjit.</h1></body></html>')})
+
+app.put('/update-data', (req,res)=>{res.send('<html lang="en"><body><h1>Data Updated successfully</h1></body></html>')})
+
+app.delete('/delete-data', (req,res)=>{res.send('<html lang="en"><body><h1>Data Deleted successfully.</h1></body></html>')})
