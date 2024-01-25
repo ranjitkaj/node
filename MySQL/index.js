0476693018c44766cf354c0ebd6b7c78be4597be
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-01-25 16:40:19 +0530

    Update index.js

--- MySQL/index.js
+++ MySQL/index.js
@@ -3,8 +3,9 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : ''
+
 });
- 
+
 connection.connect(function(err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
@@ -13,3 +14,4 @@ connection.connect(function(err) {
  
   console.log('connected as id ' + connection.threadId);
 });
+ 
