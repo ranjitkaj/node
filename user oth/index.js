c6296fb1c80f0e46ea3a7dbd36454774df4c1a92
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-02-08 14:20:33 +0530

    Update index.js

--- user oth/index.js
+++ user oth/index.js
@@ -24,13 +24,28 @@
 
 //######################
 
+
+
+
+
 const express = require('express');
+const app = express();
 const nodemailer = require('nodemailer');
 const otpGenerator = require('otp-generator');
+const captcha = require('trek-captcha');
 const ejs = require('ejs');
 path = require('path');
+fs = require('fs');
 
-const app = express();
+
+app.set('view engine', 'ejs')
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// ########   To use Static Files   ##########
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Configure Nodemailer
 const transporter = nodemailer.createTransport({
@@ -45,6 +60,8 @@ const transporter = nodemailer.createTransport({
   }
 });
 
+
+
 // Generate OTP
 const generateOTP = () => {
   return otpGenerator.generate(6, { digits: true, alphabets: false, upperCase: false, specialChars: false });
@@ -52,36 +69,23 @@ const generateOTP = () => {
 
 const otp = generateOTP();
 
-// Send OTP via Email
-app.get('/send-otp', (req, res) => {
-  const email = req.query.email;
-  //const otp = generateOTP();
-  console.log(otp)
 
-  const mailOptions = {
-    from: 'ranjitkajraitha@gmail.com',
-    to: 'kingranjit3899@gmail.com',
-    subject: 'OTP Verification',
-    text: `Your OTP is: ${otp}`
-  };
+// generate Captcha
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.status(500).send('Failed to send OTP');
-    } else {
-      console.log('OTP sent:', info.response);
-      res.status(200).send('OTP sent successfully');
-      res.render('otp', { otp });
-    }
-  });
-});
+
+async function run() {
+  const { token, buffer } = await captcha()
+  // console.log(token, buffer)
+  fs.createWriteStream('./public/a.gif').on('finish', () => console.log(token), generatecaptcha = token).end(buffer)
+  
+}     
+
+const capt = run()
+
+
+
 
 // Verify OTP on Web Page
-
-app.set('view engine', 'ejs')
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/verify-otp', (req, res) => {
@@ -100,19 +104,63 @@ app.get('/verify-otp', (req, res) => {
 //     })
 
 app.post('/verify-otp', function (req, res) {
-
-  if (req.body.otp == otp) {
+  if (req.body.otp == otp ) {
       res.send("You has been successfully registered");
   }
   else {
       res.render('incorrect otp');
   }
+
 });
+
+
+
+app.get('/register-captcha', (req, res) => {
+  res.render('register-captcha');
+})
+
+
+
+
+app.post('/register-captcha', function (req, res) {
+
+  if ( req.body.cap == generatecaptcha ) {
+    
+  const email = req.body.email;
+  //const otp = generateOTP();
+  console.log(otp)
+
+  const mailOptions = {
+    from: 'ranjitkajraitha@gmail.com',
+    to: 'kingranjit3899@gmail.com',
+    subject: 'OTP Verification',
+    text: `Your OTP is: ${otp}`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.status(500).send('Failed to send OTP');
+    } else {
+      console.log('OTP sent:', info.response);
+      res.status(200)
+      res.render( 'verify-otp');
+    }
+       //res.send("You has been successfully registered");
+      res.render( 'verify-otp');
+  }
+  
+  );
+ }
+});
+
 
 // Start the server
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
+
+
 
 
 
