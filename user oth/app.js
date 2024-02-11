aa57f83f4884adb55cd0754040f3666107c2c083
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-02-11 08:49:28 +0530

    Create app.js

---
+++ user oth/app.js
@@ -0,0 +1,57 @@
+const express = require('express')
+
+const bodyParser = require('body-parser')
+
+const { validationResult } = require('express-validator')
+
+const repo = require('./repository')
+
+const { validateConfirmPassword } = require('./validator')
+
+const signupTemplet = require('./signup')
+ 
+const app = express()
+ 
+const port = process.env.PORT || 3000
+ 
+// The body-parser middleware to parse form data
+
+app.use(bodyParser.urlencoded({ extended: true }))
+ 
+ 
+// Get route to display HTML form to sign in
+
+app.get('/signup', (req, res) => {
+
+    res.send(signupTemplet({}))
+})
+ 
+// Post route to handle form submission logic and 
+app.post(
+
+    '/signup',
+
+    [validateConfirmPassword],
+
+    async(req, res) => {
+
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+
+        return res.send(signupTemplet({ errors }))
+
+    }
+
+    const { email, password } = req.body
+
+    await repo.create({ email, password })
+
+    res.send('Sign Up successfully')
+})
+ 
+// Server setup
+app.listen(port, () => {
+
+    console.log(`Server start on port ${port}`)
+})
