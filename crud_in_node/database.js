87e161793354a20c9cd4ea6442af845a91a864a4
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-02-17 22:36:57 +0530

    Update database.js

--- crud_in_node/database.js
+++ crud_in_node/database.js
@@ -1,5 +1,17 @@
+const express = require("express");
+const app = express();
+port = 3000;
+
+app.listen(port , ()=>{
+    console.log(`app is running on port ${port}`)
+})
+
+app.set("view engine","ejs");
+app.use(express.urlencoded({extended:false}))
+
+
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/crud_in_node").then(()=>{
+mongoose.connect("mongodb+srv://ranjit:Ranjit1234@cluster0.szk3lvs.mongodb.net/?retryWrites=true&w=majority").then(()=>{
     console.log("database connected");
 }).catch((e) => {
     console.log(e);
@@ -17,14 +29,28 @@ const Schema = new mongoose.Schema({
 const Usermodel = mongoose.model("User" , Schema);
 module.exports = Usermodel;
 
-createmp = new Usermodel({
-    name: "Sujeet kumar",
-    email: "sujeetkumar@123",
-    password: "sujeet123",
-    mobile: 5689231456,
-    address: "Kahalgon",
-    salary: 15000
-}).save().then(()=>{
-    console.log("saved");
+// createmp = new Usermodel({
+//     name: "Sujeet kumar",
+//     email: "sujeetkumar@123",
+//     password: "sujeet123",
+//     mobile: 5689231456,
+//     address: "Kahalgon",
+//     salary: 15000
+// }).save().then(()=>{
+//     console.log("saved");
+
+// })
+
+
+app.get("/",(req,res)=>{
+    res.render("register");
+})
+
+app.post("/",async(req,res)=>{
+    const {name, mobile,address,salary, date, email,password}=req.body;
+    const newuser = new Usermodel({name, mobile,address,salary, date, email,password});
+    const usersave = await newuser.save();
+    console.log(`usersave ${usersave}`);
+    res.redirect("/");
 
 })
