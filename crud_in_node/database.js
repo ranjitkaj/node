1cf4e559f8c3dfa18e9bdb6e08eb0c3f85c25f72
Ranjit Kumar <121536828+ranjitkaj@users.noreply.github.com>
2024-02-27 12:31:59 +0530

    Update database.js

--- crud_in_node/database.js
+++ crud_in_node/database.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-port = 3000;
+port = process.env.PORT || 3000;
 
 app.listen(port , ()=>{
     console.log(`app is running on port ${port}`)
@@ -17,38 +17,27 @@ mongoose.connect("mongodb+srv://ranjit:Ranjit1234@cluster0.szk3lvs.mongodb.net/?
     console.log(e);
 })
 const Schema = new mongoose.Schema({
-    name:{type: String, required: true},
-    email:{type: String, required: true},
-    password:{type: String, required: true},
+    name:String,
+    email:String,
+    mobile:Number,
+    password:String,
+    cpassword:String,
     date:{type: Date, default: Date.now},
-    mobile:{type: Number},
-    address:{type: String},
-    salary:{type: Number}
+    address: String,
+    gender: String
 
 })
 const Usermodel = mongoose.model("User" , Schema);
 module.exports = Usermodel;
 
-// createmp = new Usermodel({
-//     name: "Sujeet kumar",
-//     email: "sujeetkumar@123",
-//     password: "sujeet123",
-//     mobile: 5689231456,
-//     address: "Kahalgon",
-//     salary: 15000
-// }).save().then(()=>{
-//     console.log("saved");
-
-// })
-
 
 app.get("/",(req,res)=>{
-    res.render("register");
+    res.send(register);
 })
 
 app.post("/",async(req,res)=>{
-    const {name, mobile,address,salary, date, email,password}=req.body;
-    const newuser = new Usermodel({name, mobile,address,salary, date, email,password});
+    const {name,email,cpassword, date, mobile,address}=req.body;
+    const newuser = new Usermodel({name, email, cpassword, date, mobile,address});
     const usersave = await newuser.save();
     console.log(`usersave ${usersave}`);
     res.redirect("/");
